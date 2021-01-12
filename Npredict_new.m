681abf6f8a649f22f9aaa56c672c89db5e594539
Avra98 <69806857+Avra98@users.noreply.github.com>
2021-01-13 03:36:29 +0530

    Add files via upload
    Joint heat source reconsturction random trials

--- Npredict_new.m
+++ Npredict_new.m
@@ -1,36 +1,26 @@
-function [N0,upred]= Npredict(meas,N0,A)
+function [Npred,upred]= Npredict(meas,N0,A)
 % N0 is the intial guess of N.
-[p,d,~]=svd(A);
-eps=100; %l2 ball constraint 
-for i=1:20
+%[p,d,~]=svd(A);
+%eps=0.1; %l2 ball constraint 
+iter=13;
+Npred=zeros(1,iter);
+for i=1:iter
 M=(A^(N0));
 penalty = 'MCP';
-if i<10
-  %%%Either use MCP penality minimiser or cvx minimiser. Comment out the other one.
-   upred = lsq_sparsereg(M,meas,2,'penalty',penalty,'penparam',500);
-    upred = l1opt(M,meas,eps);
-else
-    upred = lsq_sparsereg(M,meas,2,'penalty',penalty,'penparam',2000);
-    upred = l1opt(M,meas,eps);
-end
+%penalty= 'POWER';
+
+   upred = lsq_sparsereg(M,meas,1,'penalty',penalty,'penparam',100);
+   %upred = lsq_sparsereg(M,meas,1,'penalty',penalty,'penparam',1);
+    %upred= l1opt(M,meas,0.5);
 
 %%Either use diagsolve or iterative l2min. For noise measurements, use
 %%iterative l2min. Comment out the other one. 
+%N0 =diagsolve_new(p,meas,upred,diag(d));  %%Use for diagsolve using polyfit
+ [~,N0]=l2min(meas,upred,A);      %% Use for iterative l2 min of(A^n.u(0)-u(n))
+ Npred(i)=N0;
 
-N0 =diagsolve_new(p,meas,upred,diag(d));
-N0=l2min(meas,upred,A)      %% Use for iterative l2 min of(A^n.u(0)-u(n))
-
-disp(['N0:',num2str(N0)])
+disp(['N0:',num2str(N0)]);
 figure(1);plot(upred); pause(.2);
 end
-%[n1,~]=findn(D,d);
-% M1=S*(A^(n1));
-% upred1 = lsq_sparsereg(M1,meas,2,'penalty',penalty,'penparam',500);
-% D1=diagsolve(p,upred1,u0); 
-% [n2,~]=findn(D1,d);
-% M2=S*(A^(n2));
-% upred2 = lsq_sparsereg(M2,meas,2,'penalty',penalty,'penparam',500);
-% D2=diagsolve(p,upred2,u0); 
-% [n3,~]=findn(D2,d);
-% N_opt=n3; 
+
 end
---
+++ mcpitercheck.m
@@ -0,0 +1,47 @@
+Dx=1; % step size
+%DY=0.1;
+Nx=1000;
+%Ny=5;
+X=1:Dx:Nx;
+%Y=0:DY:Ny;=127; % arbitrary thermal diffusivity (units in mm^2/sec)
+alpha_silver=165.6;
+alpha_al=97;
+alpha_si=88;
+alpha_fe=23;
+alpha_air=19;
+alpha=25;
+%--boundary conditions----------------------------------------------------
+U(1:Nx) = 0 ;
+s(1:1000)=1/(2*alpha);
+
+Ncount=500;
+
+%%Consider Nx interior points, two boundary points at the extreme. 
+A=zeros(Nx,Nx);
+
+%Filling up intereior pointa
+for i=2:Nx-1
+        A(i,i)=1-2*s(i);
+        A(i,i-1)=s(i);
+        A(i,i+1)=s(i);
+end
+
+%%Dirchlet boundary conditions 
+A(1,:)=zeros(1,Nx);
+A(Nx,:)=zeros(1,Nx);
+A(:,1)=zeros(1,Nx);
+A(:,Nx)=zeros(1,Nx);
+
+
+k=3;
+trails=10;
+Npred=zeros(1,35,trails);
+for (i=1:trails)
+   U(randperm(Nx-2,k)+1)=100;
+   U=abs(U);
+   Unit=U;
+   meas=(A^Ncount)*Unit';
+   [Npred(:,:,i),~] = Npredict_new(meas,200,A);
+end
+
+   
