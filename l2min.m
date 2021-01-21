3fdaa1a6392c0e2f67e205b57ea3f0c54789c748
unknown <rongwan6@msu.edu>
2021-01-21 16:37:51 -0500

    updated l2min

--- l2min.m
+++ l2min.m
@@ -1,8 +1,9 @@
-function k=l2min(u,u0,A)
+function [err,k]=l2min(u,u0,A)
 n=size(u0,1);
 B=eye(n);
+u0 = A*u0;
 for(i=1:5000)
-     e(i)=norm(u-B*u0);
-     B=B*A;
+     e(i)=norm(u-u0);
+     u0=A*u0;
 end
 [err,k]=min(e);
